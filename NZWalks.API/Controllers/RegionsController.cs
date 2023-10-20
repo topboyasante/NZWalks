6a736dbb4cae01b8fa69ba80289823a4218225c7
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-10-20 21:13:24 +0000

    Set up models, dbContext

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        [HttpGet] 
+        public IActionResult GetAllRegions() {
+            var regions = new List<Region>()
+            {
+                new Region
+                {
+                    Name= "Wellington"
+                }
+            };
+            return Ok(regions);
+        }
+    }
+}
